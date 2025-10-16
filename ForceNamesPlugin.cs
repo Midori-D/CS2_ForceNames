83189d6ee7c4a86b1db8d3fe343692026d72f708
Midori <cosgnggi@naver.com>
2025-10-16 19:39:24 +0900

    Update ForceNamesPlugin.cs

--- ForceNamesPlugin.cs
+++ ForceNamesPlugin.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
